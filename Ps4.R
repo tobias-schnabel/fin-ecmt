da9bcaae70032cb4de37fe86a0774d37928bc28f
Tobias Schnabel <ctobiasschnabel@gmail.com>
2022-10-28 16:19:58 -0500

    plots for 7.3, 8.3, 7.10

--- Ps4.R
+++ Ps4.R
@@ -11,10 +11,69 @@ library(MASS)
 library(glmnet)
 library(pls)
 library(lmvar)
+library(leaps)
+
+#Ch 7 Ex 3
+x = -2:2
+y = 1 + x + -2 * (x-1)^2 * I(x>1)
+
+setwd("/Users/ts/Dropbox/Apps/Overleaf/FIN ECMT HW/Figures/HW4")
+png("plot1.png", width = 800, height = 800, units = "px")
+plot(x, y)
+dev.off()
+setwd("/Users/ts/Git/fin-ecmt")
 
 #Ch 7 Ex 10
 
+attach(College)
+train = sample(length(Outstate), length(Outstate)/2)
+test = -train
+College.train = College[train, ]
+College.test = College[test, ]
+reg.fit = regsubsets(Outstate ~ ., data = College.train, nvmax = 17, method = "forward")
+reg.summary = summary(reg.fit)
+par(mfrow = c(1, 3))
+
+setwd("/Users/ts/Dropbox/Apps/Overleaf/FIN ECMT HW/Figures/HW4")
+png("plot3.png", width = 800, height = 800, units = "px")
+plot(reg.summary$cp, xlab = "Number of Variables", ylab = "Cp", type = "l")
+min.cp = min(reg.summary$cp)
+std.cp = sd(reg.summary$cp)
+abline(h = min.cp + 0.2 * std.cp, col = "red", lty = 2)
+abline(h = min.cp - 0.2 * std.cp, col = "red", lty = 2)
+dev.off()
+
+png("plot4.png", width = 800, height = 800, units = "px")
+plot(reg.summary$bic, xlab = "Number of Variables", ylab = "BIC", type = "l")
+min.bic = min(reg.summary$bic)
+std.bic = sd(reg.summary$bic)
+abline(h = min.bic + 0.2 * std.bic, col = "red", lty = 2)
+abline(h = min.bic - 0.2 * std.bic, col = "red", lty = 2)
+dev.off()
+
+png("plot5.png", width = 800, height = 800, units = "px")
+plot(reg.summary$adjr2, xlab = "Number of Variables", ylab = "Adjusted R2", 
+     type = "l", ylim = c(0.4, 0.84))
+max.adjr2 = max(reg.summary$adjr2)
+std.adjr2 = sd(reg.summary$adjr2)
+abline(h = max.adjr2 + 0.2 * std.adjr2, col = "red", lty = 2)
+abline(h = max.adjr2 - 0.2 * std.adjr2, col = "red", lty = 2)
+dev.off()
+setwd("/Users/ts/Git/fin-ecmt")
+
+
+
 #Ch 8 Ex 3
+p = seq(0, 1, 0.01)
+gini = p * (1 - p) * 2
+entropy = -(p * log(p) + (1 - p) * log(1 - p))
+class.err = 1 - pmax(p, 1 - p)
+
+setwd("/Users/ts/Dropbox/Apps/Overleaf/FIN ECMT HW/Figures/HW4")
+png("plot2.png", width = 800, height = 800, units = "px")
+matplot(p, cbind(gini, entropy, class.err), col = c("red", "green", "blue"))
+dev.off()
+setwd("/Users/ts/Git/fin-ecmt")
 
 #Ch 8 Ex 10
 
