0fb6c2b966d39cd284b18c6e805784b33132b95c
Tobias Schnabel <ctobiasschnabel@gmail.com>
2022-11-27 16:39:04 -0600

    solved d

--- PS5.R
+++ PS5.R
@@ -31,3 +31,19 @@ summary(tune.out)
 #9.7(c)
 tune.outd<-tune(svm, mpglevel~.,data = Auto, kernel ="polynomial", ranges = list(cost = c(0.01, 0.1, 1, 5, 10, 100), degree = c(2,3,4))) 
 summary(tune.outd)
+
+tune.outg<-tune(svm, mpglevel~., data = Auto, kernel ="radial",ranges = list(cost = c(0.01, 0.1, 1, 5, 10, 100), gamma = c(0.01, 0.1, 1, 5, 10, 100)))
+summary(tune.outg)
+
+#9.7(d)
+svm.linear <- svm(mpglevel~., data = Auto, kernel = "linear", cost=1)
+svm.poly <- svm(mpglevel~., data = Auto, kernel = "polynomial",cost = 100, degree = 2)
+svm.radial <- svm(mpglevel~., data = Auto, kernel = "radial", cost= 100, gamma = 0.01)
+plotpairs = function(fit){
+  for (name in names(Auto)[!(names(Auto) %in% c("mpg", "mpglevel","name"))]) {
+    plot(fit, Auto, as.formula(paste("mpg~", name, sep="")))
+  }
+}
+plotpairs(svm.linear)
+plotpairs(svm.poly)
+plotpairs(svm.radial)
