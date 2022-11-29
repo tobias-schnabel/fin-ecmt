31800a8cb6a82d2de48d423192a2a8fa36b319d5
Tobias Schnabel <ctobiasschnabel@gmail.com>
2022-11-28 20:40:42 -0600

    added template files for final exam

---
+++ Final_Exam_Classification.R
@@ -0,0 +1,20 @@
+this.dir=getwd()
+setwd(this.dir)
+rm(list=ls())
+
+set.seed(1)
+CDTraining=read.table('Classification_Training.csv',sep=',',header=TRUE)
+CDTest=read.table('Classification_Test.csv',sep=',',header=TRUE)
+
+#Combine training dataset and test dataset
+ExamData=rbind(CDTraining,CDTest)
+
+attach(ExamData)
+
+#Index of training set and testing set
+train=1:dim(CDTraining)[1]
+test=(dim(CDTraining)[1]+1):dim(ExamData)[1]
+
+#Dimension of covariates
+x=model.matrix(y~.,ExamData)[,-1]
+p=ncol(x)
---
+++ Final_Exam_Regression.R
@@ -0,0 +1,26 @@
+this.dir=getwd()
+setwd(this.dir)
+rm(list=ls())
+
+set.seed(1)
+RDTraining=read.table('Regression_Training.csv',sep=',',header=TRUE)
+RDTest=read.table('Regression_Test.csv',sep=',',header=TRUE)
+
+#Combine training dataset and test dataset
+ExamData=rbind(RDTraining,RDTest)
+
+attach(ExamData)
+
+#Index of training set and testing set
+train=1:dim(RDTraining)[1]
+test=(dim(RDTraining)[1]+1):dim(ExamData)[1]
+
+#Dimension of covariates
+x=model.matrix(y~.,ExamData)[,-1]
+p=ncol(x)
+
+#Linear Model
+LM.fit=lm(y~.,data=ExamData,subset=train)
+LM.pred=predict(LM.fit,ExamData[-train,])
+LM.MSE=mean((y[-train]-LM.pred)^2)
+print(c('Linear model',LM.MSE))
