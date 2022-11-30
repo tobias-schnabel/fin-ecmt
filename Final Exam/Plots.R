f8acbeebfb66a80a4f24b7fefc5400f9bf0d28a9
Tobias Schnabel <ctobiasschnabel@gmail.com>
2022-11-30 16:28:04 -0600

    added 2 plots

--- Final Exam/Plots.R
+++ Final Exam/Plots.R
@@ -3,3 +3,52 @@
 if (Sys.info()[7] == "ts") {
   setwd('/Users/ts/Dropbox/Apps/Overleaf/FIN ECMT Final Assignment/Figures')
 }
+
+# Forward Stepwise Selection
+png("fss.png", width = 800, height = 800, units = "px")
+fss.summary = summary(fss)
+par(mfrow = c(1, 3))
+plot(fss.summary$cp, xlab = "# of Variables", ylab = "Mallow's Cp", type = "l")
+min.cp = min(fss.summary$cp)
+std.cp = sd(fss.summary$cp)
+abline(h = min.cp + 0.1 * std.cp, col = "red", lty = 2)
+abline(h = min.cp - 0.1 * std.cp, col = "red", lty = 2)
+
+plot(fss.summary$bic, xlab = "# of Variables", ylab = "BIC", type = "l")
+min.bic = min(fss.summary$bic)
+std.bic = sd(fss.summary$bic)
+abline(h = min.bic + 0.1 * std.bic, col = "red", lty = 2)
+abline(h = min.bic - 0.1 * std.bic, col = "red", lty = 2)
+
+plot(fss.summary$adjr2, xlab = "# of Variables", ylab = "Adjusted R2",
+     type = "l", ylim = c(0.4, 0.84))
+max.adjr2 = max(fss.summary$adjr2)
+std.adjr2 = sd(fss.summary$adjr2)
+abline(h = max.adjr2 + 0.1 * std.adjr2, col = "red", lty = 2)
+abline(h = max.adjr2 - 0.1 * std.adjr2, col = "red", lty = 2)
+dev.off()
+
+# Forward Stepwise Selection
+png("bss.png", width = 800, height = 800, units = "px")
+
+bss.summary = summary(bss)
+par(mfrow = c(1, 3))
+plot(bss.summary$cp, xlab = "# of Variables", ylab = "Mallow's Cp", type = "l")
+min.cp = min(bss.summary$cp)
+std.cp = sd(bss.summary$cp)
+abline(h = min.cp + 0.1 * std.cp, col = "red", lty = 2)
+abline(h = min.cp - 0.1 * std.cp, col = "red", lty = 2)
+
+plot(bss.summary$bic, xlab = "# of Variables", ylab = "BIC", type = "l")
+min.bic = min(bss.summary$bic)
+std.bic = sd(bss.summary$bic)
+abline(h = min.bic + 0.1 * std.bic, col = "red", lty = 2)
+abline(h = min.bic - 0.1 * std.bic, col = "red", lty = 2)
+
+plot(bss.summary$adjr2, xlab = "# of Variables", ylab = "Adjusted R2",
+     type = "l", ylim = c(0.4, 0.84))
+max.adjr2 = max(bss.summary$adjr2)
+std.adjr2 = sd(bss.summary$adjr2)
+abline(h = max.adjr2 + 0.1 * std.adjr2, col = "red", lty = 2)
+abline(h = max.adjr2 - 0.1 * std.adjr2, col = "red", lty = 2)
+dev.off()
